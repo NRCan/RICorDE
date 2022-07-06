a72e3afd5292f57bb05c6cc49767b21a67041fd5
cefect <cefect@gmail.com>
2022-07-06 12:30:19 +0200

    Update __init__.py
    docstring

---
+++ __init__.py
@@ -0,0 +1,26 @@
+"""
+A nice depths tool
+
+
+ 
+#===============================================================================
+# resolution 
+#===============================================================================
+see 'RICorDE - notes.xlsx'
+
+
+#===============================================================================
+# TODO
+#===============================================================================
+ 
+
+paralleleize expensive loops
+
+add some better logic checks (%inundation)
+
+create a master parameter list  (documentation)
+    explan function/roll of each parameter
+    which functions use the parameter
+    default value
+
+"""

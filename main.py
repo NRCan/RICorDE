b3ffe09e91fcd1c3ad104e74340774df75066cfb
cefect <cefect@gmail.com>
2022-07-06 18:57:12 +0200

    Update main.py
    add argparser

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 '''
 Running RICorDE from command line    
 
-TODO:
-    need a parameter file
- 
-    switch to data file request/call dictionary
-        add metadata per-datafile for this
-        add function kwargs
-        
-    clean up the creation/use of temporary folders
-        
-    collapse all necessary hp and tcom ricorde into a single hp.py
-    re-org folders
-        RICorDE
-            main #top-level callers
-            can_data
-            hand #pre-process DEM
-            imax #hydrauilc maximum
-            mosaic
-            hp #all common helpers
-            
-    parallelize
 
 
 ''' 
 import sys, argparse
-from ricorde.runrs import runr
+from ricorde.runrs import run_from_params
+from hp.basic import get_dict_str
  
 
 if __name__ == "__main__":
+    """getting some 'application path not initlizlized?"""
     print(sys.argv)
     #===========================================================================
     # setup argument parser 
     #===========================================================================
-    parser = argparse.ArgumentParser(prog='RICorDE',description='execute RICorDE')
-    #add arguments
+    parser = argparse.ArgumentParser(prog='RICorDE',description='Compute a depths grid from flood inundation and DEM')
+    #===========================================================================
+    # #add arguments
+    #===========================================================================
+    parser.add_argument("param_fp",help='parameter file path') #positional 
  
-    parser.add_argument("-tag",'-t', help='tag for the run') #this defaults to None if not passed
-    parser.add_argument("-write",'-w', help='flag to write outputs', action='store_true')#defaults to False
+    #scripts.Session
+    parser.add_argument("-exit_summary", help='flag to disable writing the exit summary', action='store_true')#defaults to False
     
-    parser.add_argument("-cap", help='cap to apply to beach values', type=float, default=6.0) #this defaults to None if not passed
-    parser.add_argument("-floor", help='cap to apply to beach values', type=float, default=1.5) #this defaults to None if not passed
-    parser.add_argument("-dev",'-d', help='flag for dev runs', action='store_true')
-    parser.add_argument("-b2_method",  help='method for beach2 construction', default='pixels')
-    parser.add_argument("-hgi_resolution",  help='resolution for hgInterp', default=90*4, type=int)
-    parser.add_argument("-hgi_minPoints",  help='minimum points count (pts_cnt) for hgInterp', default=5, type=int)
-    parser.add_argument("-hval_precision",  help='precision for HAND values in hgSmooth', default=0.2, type=float)
+    #hp.Q.Qproj
+    parser.add_argument("-compress",'-c', help='set the default raster compression level', 
+                        choices=['hiT', 'hi', 'med', 'none'], default='med')
+    
+    
+    #hp.oop.Basic
+    parser.add_argument("-root_dir",'-rd', help='Base directory of the project. Used for generating default directories. Defaults to value in definitions', 
+                        default=None)  
+    parser.add_argument("-out_dir",'-od', 
+                        help='Directory used for outputs. Defaults to a sub-directory within root_dir', 
+                        default=None) 
+    parser.add_argument("-temp_dir", 
+                        help='Directory for temporary outputs (i.e., cache). Defaults to a sub-directory of out_dir.', 
+                        default=None) 
+    parser.add_argument("-tag",'-t', help='tag for the run', default='r0') 
+    parser.add_argument("-write",'-w', help='flag to write outputs', action='store_true')#defaults to False
+    parser.add_argument("-name",'-n', help='project name', default='proj1')
+    parser.add_argument("-prec", help='Default float precision', default=None, type=int)
+    parser.add_argument("-overwrite", help='Default behavior when attempting to overwrite a file', action='store_false')
+    parser.add_argument("-relative", help='Default behavior of filepaths (relative vs. absolute)', action='store_true')
     
     args = parser.parse_args()
     kwargs = vars(args)
-    print('parser got these kwargs: \n    %s'%kwargs) #print all the parsed arguments in dictionary form
+    print('parser got these kwargs: \n    %s'%get_dict_str(kwargs)) #print all the parsed arguments in dictionary form
     
-     
-    dev = kwargs.pop('dev')
-    print('\n\nSTART (dev=%s) \n\n\n\n'%dev)
+ 
+    print('\n\nSTART \n\n\n\n')
+    #run_from_params(**kwargs)

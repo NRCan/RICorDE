8eff2383ab96a72b7e4a2bcead3ea824856c53e0
Seth Bryant <bryant.seth@gmail.com>
2023-04-14 14:06:37 -0600

    clean up tutorial

--- tutorials/bryant2022_fred12/run_tutorial.bat
+++ tutorials/bryant2022_fred12/run_tutorial.bat
@@ -1,20 +1,21 @@
 REM =============
 REM VARIABLES
 REM =============
-set PARAM_FP=C:\LS\09_REPOS\03_TOOLS\RICorDE\tutorials\bryant2022_fred12\RICorDE_bryant2022.ini
+::specify parameter file path
+set PARAM_FP=%~dp0RICorDE_bryant2022.ini
+
+::specify root directory for building outputs, loggers, etc.
 set ROOT_DIR=C:\LS\10_IO\ricorde\bryant2022\
 
 REM =============
 REM SETUP
 REM =============
 REM Activate pyqgis environment and 
-call "C:\Users\cefect\.venv\QGIS 3.22.8\ricorde\activate.bat"
+call "%~dp0..\..\dev\activate_py.bat"
 
-REM add the project to the python path
-set PYTHONPATH=C:\LS\09_REPOS\03_TOOLS\RICorDE;%PYTHONPATH%
-
+ 
 REM set the path to RICorDE's main caller
-set MAIN=C:\LS\09_REPOS\03_TOOLS\RICorDE\main.py
+set MAIN=%SRC_DIR%\main.py
 
 REM =============
 REM EXECUTE 
@@ -22,4 +23,4 @@ REM =============
 ECHO Executing RICorDE
 python %MAIN% %PARAM_FP% -rd %ROOT_DIR% -t r1 
 
-pause
+cmd /k
